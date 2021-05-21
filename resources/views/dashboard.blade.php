5457ad35e464f3757148f1ee549e3121b74d1bb4
EsraArslan33 <esra_arslan_33@hotmail.com>
2021-05-21 19:06:30 +0300

    layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+      ANASAYFA
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="alert alert-danger">esra arslannnnnnn</div>
-            </div>
-        </div>
-    </div>
+DSGFDRFG
+    <div class="alert alert-danger">danger</div>
 </x-app-layout>
